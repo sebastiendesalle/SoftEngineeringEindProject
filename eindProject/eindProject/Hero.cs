ac76f6fe6f2c1e5d3b1946cadebf0a9a9f2ae8dd
sebastien <sebastien.desalle@student.ap.be>
2025-11-23 13:55:26 +0100

    Add multiple states

--- eindProject/eindProject/Hero.cs
+++ eindProject/eindProject/Hero.cs
@@ -9,7 +9,10 @@ namespace eindProject
     internal class Hero : IGameObject
     {
         private Texture2D heroTexture;
-        private Animation animation;
+
+        // to store multiple animations
+        private Dictionary<string, Animation> animations;
+        private Animation currentAnimation;
 
         private IInputReader inputReader;
         private PhysicsComponent physics;
@@ -18,6 +21,7 @@ namespace eindProject
         private float scale;
         private Vector2 spriteSize;
         private Vector2 drawOffset;
+        private SpriteEffects flipEffect = SpriteEffects.None; // to face left/right
 
         public Hero(Texture2D texture, IInputReader inputReader, List<Rectangle> obstacles, Rectangle bounds, Vector2 startPos, float scale = 5f)
         {
@@ -25,15 +29,40 @@ namespace eindProject
             this.inputReader = inputReader;
             this.scale = scale;
 
+            animations = new Dictionary<string, Animation>();
 
-            // use sprite coords
-            // TODO: fix hitbox (too big)
-            animation = new Animation();
-            animation.AddFrame(new AnimationFrame(new Rectangle(0, 64, 64, 64)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(64, 64, 64, 64)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(128, 64, 64, 64)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(192, 64, 64, 64)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(256, 64, 64, 64)));
+            // idle animation
+            var idleAnim = new Animation();
+            idleAnim.AddFrame(new AnimationFrame(new Rectangle(0, 0, 64, 64)));
+            idleAnim.AddFrame(new AnimationFrame(new Rectangle(64, 0, 64, 64)));
+            idleAnim.AddFrame(new AnimationFrame(new Rectangle(128, 0, 64, 64)));
+            idleAnim.AddFrame(new AnimationFrame(new Rectangle(192, 0, 64, 64)));
+            animations.Add("Idle", idleAnim);
+
+            // walk animation
+            var walkAnim = new Animation();
+            walkAnim.AddFrame(new AnimationFrame(new Rectangle(0, 64, 64, 64)));
+            walkAnim.AddFrame(new AnimationFrame(new Rectangle(64, 64, 64, 64)));
+            walkAnim.AddFrame(new AnimationFrame(new Rectangle(128, 64, 64, 64)));
+            walkAnim.AddFrame(new AnimationFrame(new Rectangle(192, 64, 64, 64)));
+            walkAnim.AddFrame(new AnimationFrame(new Rectangle(256, 64, 64, 64)));
+            walkAnim.AddFrame(new AnimationFrame(new Rectangle(320, 64, 64, 64)));
+            animations.Add("Walk", walkAnim);
+
+            // jump animation
+            var jumpAnim = new Animation();
+
+            // frames from row 10
+            jumpAnim.AddFrame(new AnimationFrame(new Rectangle(0, 576, 64, 64)));
+            jumpAnim.AddFrame(new AnimationFrame(new Rectangle(64, 576, 64, 64)));
+
+            // frames from row 11
+            jumpAnim.AddFrame(new AnimationFrame(new Rectangle(0, 640, 64, 64)));
+            jumpAnim.AddFrame(new AnimationFrame(new Rectangle(64, 640, 64, 64)));
+            animations.Add("Jump", jumpAnim);
+
+            // set default state
+            currentAnimation = animations["Idle"];
 
             // calc hitbox size
             float hitBoxWidth = 30 * scale;
@@ -41,14 +70,12 @@ namespace eindProject
             spriteSize = new Vector2(hitBoxWidth, hitBoxHeight); // physics size
 
             // calc offset
-            var src = animation.CurrentFrame.SourceRectangle;
-            float visualWidth = src.Width * scale;
-            float visualHeight = src.Height * scale;
-
+            var src = currentAnimation.CurrentFrame.SourceRectangle;
             drawOffset = new Vector2(
-                (visualWidth - hitBoxWidth) / 2f, // center X
-                visualHeight - hitBoxHeight // center Y
+                (src.Width * scale - hitBoxWidth) / 2f, // center X
+                (src.Height * scale) - hitBoxHeight // center Y
             );
+
             physics = new PhysicsComponent(startPos, obstacles, bounds);
         }
 
@@ -64,7 +91,37 @@ namespace eindProject
 
             // update physics
             physics.Update(delta, (int)spriteSize.X, (int)spriteSize.Y);
-            animation.Update(gameTime);
+
+            // state machine
+            UpdateAnimationState(input);
+
+            // update animation framecounter
+            currentAnimation.Update(gameTime);
+        }
+
+        private void UpdateAnimationState(Vector2 input)
+        {
+            if (!physics.IsGrounded)
+            {
+                currentAnimation = animations["Jump"]; // set jump animation
+            }
+            else if (input.X != 0)
+            {
+                currentAnimation = animations["Walk"]; // set walk animation
+            }
+            else
+            {
+                currentAnimation = animations["Idle"]; // set idle animation
+            }
+
+            if (input.X > 0)
+            {
+                flipEffect = SpriteEffects.None; // face right
+            }
+            else if (input.X < 0)
+            {
+                flipEffect = SpriteEffects.FlipHorizontally; // face left
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -74,12 +131,12 @@ namespace eindProject
             spriteBatch.Draw(
                 heroTexture,
                 drawPosition,
-                animation.CurrentFrame.SourceRectangle,
+                currentAnimation.CurrentFrame.SourceRectangle,
                 Color.White,
                 0f,
                 Vector2.Zero,
                 scale,
-                SpriteEffects.None,
+                flipEffect,
                 0f
             );
         }

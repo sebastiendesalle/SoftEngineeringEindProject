bb56240aa76021f17ec1984c67d0f7ac6fd5e241
sebastien <sebastien.desalle@student.ap.be>
2025-11-23 13:17:04 +0100

    Fix hero hitbox size

--- eindProject/eindProject/Hero.cs
+++ eindProject/eindProject/Hero.cs
@@ -17,6 +17,7 @@ namespace eindProject
 
         private float scale;
         private Vector2 spriteSize;
+        private Vector2 drawOffset;
 
         public Hero(Texture2D texture, IInputReader inputReader, List<Rectangle> obstacles, Rectangle bounds, Vector2 startPos, float scale = 5f)
         {
@@ -34,10 +35,20 @@ namespace eindProject
             animation.AddFrame(new AnimationFrame(new Rectangle(192, 64, 64, 64)));
             animation.AddFrame(new AnimationFrame(new Rectangle(256, 64, 64, 64)));
 
-            // calc size
-            var src = animation.CurrentFrame.SourceRectangle;
-            spriteSize = new Vector2(src.Width * scale, src.Height * scale);
+            // calc hitbox size
+            float hitBoxWidth = 30 * scale;
+            float hitBoxHeight = 50 * scale;
+            spriteSize = new Vector2(hitBoxWidth, hitBoxHeight); // physics size
 
+            // calc offset
+            var src = animation.CurrentFrame.SourceRectangle;
+            float visualWidth = src.Width * scale;
+            float visualHeight = src.Height * scale;
+
+            drawOffset = new Vector2(
+                (visualWidth - hitBoxWidth) / 2f, // center X
+                visualHeight - hitBoxHeight // center Y
+            );
             physics = new PhysicsComponent(startPos, obstacles, bounds);
         }
 
@@ -58,9 +69,11 @@ namespace eindProject
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            Vector2 drawPosition = physics.Position - drawOffset;
+
             spriteBatch.Draw(
                 heroTexture,
-                physics.Position, // get position from component
+                drawPosition,
                 animation.CurrentFrame.SourceRectangle,
                 Color.White,
                 0f,
